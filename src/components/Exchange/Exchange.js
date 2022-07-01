1725ffecc38ac298bc8f843a1dbcfcac75ae4685
Namat Bayamanov <namataxvd15@gmail.com>
2022-07-01 21:41:43 +0600

    created new components

---
+++ src/components/Exchange/Exchange.js
@@ -0,0 +1,9 @@
+function Exchange() {
+  return ( 
+    <div>
+    
+    </div>
+  );
+}
+
+export default Exchange;
---
+++ src/components/Exchange/Exchange2/Exchange2.js
@@ -0,0 +1,9 @@
+function Exchange2() {
+  return ( 
+    <div>
+    
+    </div>
+  );
+}
+
+export default Exchange2;
